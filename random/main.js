d21412377a4a81b2ac7914e9a2cfdb4f0fc92fbe
kyanachana <tomomin001104@gmail.com>
2020-10-17 10:53:31 +0900

    repeat

--- random/main.js
+++ random/main.js
@@ -1,5 +1,7 @@
-var syugo = ["かないです。","あやだです。","てしんです。"]
+var syugo = ["かないさん、","あやださん、","てしんさん、"]
 var topic = ["今日のおやつはなんですか？","ねむいです","おなかすきました", '昨日晩ご飯何食べた？', '今正直好きな子おる？', 'コロナっていつ終わるん？', 'ガッキー派？浜辺美波派？', '自慢話してみよか', '今の雰囲気にぴったりの音楽流して', '座右の名教えて'];
 
+for(let i = 0; i<3;i++){
+    console.log(syugo[Math.floor(Math.random()*syugo.length)]+topic[Math.floor(Math.random()*topic.length)])
 
-console.log(syugo[Math.floor(Math.random()*syugo.length)]+topic[Math.floor(Math.random()*topic.length)])
+}
