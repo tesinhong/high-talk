27a3ce0633ad30b9e2759f38beee40dd319be0a6
kyanachana <tomomin001104@gmail.com>
2020-10-17 10:40:53 +0900

    syugo

--- random/main.js
+++ random/main.js
@@ -1,3 +1,4 @@
+var syugo = ["かないです。","あやだです。","てしんです。"]
 var topic = ["今日のおやつはなんですか？","ねむいです","おなかすきました"];
 
-console.log(topic[Math.floor(Math.random()*topic.length)])
+console.log(syugo[Math.floor(Math.random()*syugo.length)]+topic[Math.floor(Math.random()*topic.length)])
